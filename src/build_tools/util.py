54ef293f611ca459fd5f9afae46a1d6cd97841b7
hanya <hanya.runo@gmail.com>
2017-05-17 10:35:38 +0000

    To detect Haiku as Linux

--- src/build_tools/util.py
+++ src/build_tools/util.py
@@ -56,7 +56,7 @@ def IsMac():
 
 def IsLinux():
   """Returns true if the platform is Linux."""
-  return os.name == 'posix' and os.uname()[0] == 'Linux'
+  return os.name == 'posix' and os.uname()[0] in ('Linux', 'Haiku')
 
 
 def GetNumberOfProcessors():
